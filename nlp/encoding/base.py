23e2789ff80bdbbed38447854e5e4aea763f0200
drakai96 <aidragon1996@gmail.com>
2024-09-26 01:40:49 +0700

    Add doc string for bag_of_word.py file

--- nlp/encoding/base.py
+++ nlp/encoding/base.py
@@ -1,3 +1,6 @@
+"""
+Base class for encoding
+"""
 from abc import ABC, abstractmethod
 from typing import Dict, List, Tuple
 
@@ -7,6 +10,14 @@ from pyvi import ViTokenizer
 
 
 class BaseEncoder(ABC):
+    """
+       Abstract base class for text encoders, which can tokenize and transform
+       text documents into vector embeddings.
+
+       Attributes:
+           __AtriPyvi__ (bool): Indicates whether the PyVi library is used
+               for tokenizing sentences.
+    """
     __AtriPyvi__: bool = Field(
         default=True,
         description="Check is use pyvi library\
@@ -19,7 +30,7 @@ class BaseEncoder(ABC):
 
     def tokenizer_documents(self, documents: List[str] = None, is_pyvi: bool = True):
         """
-
+        Tokenizes the input documents using PyVi or a simple split method.
         Args:
             documents: Document of sentence default = False
             is_pyvi: bool, default = True
@@ -55,19 +66,20 @@ class BaseEncoder(ABC):
         unknown_token="#sep",
     ) -> Tuple[Dict[str, int], Dict[int, str]]:
         """
-
+        Fit documents to vocab corpus and invert vocab corpus
         Args:
             is_pyvi: bool, default = True
                 Mean True if used pyvi library to tokenizer
             vocab_cached_path: str default = nlp/cached/vocab.json
                 Mean path to save cached vocab
             use_cached: bool, default = False
-                Mean True if need to used cached to load vocab or save vocab
+                Mean True if used cached to load vocab or save vocab
             unknown_token: str = #sep
                 Mean token not in vocab change to #sep
 
-        Returns:Dict[str, int], Dict[int, str]
-            vocab and invert vocab
+        Returns:
+            Tuple[Dict[str, int], Dict[int, str]]: A tuple containing the vocabulary
+            (word-to-index mapping) and its inverse (index-to-word mapping).
 
         """
         pass
@@ -75,7 +87,7 @@ class BaseEncoder(ABC):
     @abstractmethod
     def transform(self, docs: List[str,], less_memory: True) -> Tuple | pd.DataFrame:
         """
-
+        Transforms the input documents into vector embeddings.
         Args:
             docs:
             less_memory:
@@ -86,7 +98,7 @@ class BaseEncoder(ABC):
 
     def __transform_sentence(self, sentence: str, is_pyvi=True):
         """
-
+        Transforms a single sentence into a vector representation.
         Args:
             sentence: Sentence need to embedding
             is_pyvi: bool = True
@@ -107,4 +119,3 @@ class BaseEncoder(ABC):
         Returns:
             convert idx to sentence
         """
-        pass
